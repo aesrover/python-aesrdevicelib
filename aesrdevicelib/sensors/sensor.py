750b9bf2a3441ae12cdcae6f4d8d480b0951dbe3
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-28 10:18:53 -0400

    Add device availability check in sensor base class

--- aesrdevicelib/sensors/sensor.py
+++ aesrdevicelib/sensors/sensor.py
@@ -4,3 +4,6 @@ class Sensor(object):
     def __init__(self, i2cAddress, bus= 1):
         self.bus = smbus.SMBus(bus)
         self.i2cAddress = i2cAddress
+        
+        # Test if device is connected:
+        self.bus.read_byte(i2cAddress)
