1662a9d59f7e1c6f70fbb92306bc3f61b0ad9d3f
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-07-06 14:24:25 -0400

    Fix sleep amount in ADS1115 library

--- aesrdevicelib/sensors/ads1115.py
+++ aesrdevicelib/sensors/ads1115.py
@@ -10,14 +10,16 @@ class ADS1115(sensor.Sensor):
         super(ADS1115, self).__init__(i2cAddress, *args, **kwargs)
     
     def read(self):
+        dataRate = 128
         # Write two bytes to the config register 
         self.bus.write_word_data(self.i2cAddress, 0x01, 0b0100000010000011)
         # This config: Enables continuos conversion, Disables comparator,
         # Sets the analog voltage range to 0 - 6V, and much more!
         # https://cdn-shop.adafruit.com/datasheets/ads1115.pdf (18-19)        
         
-        # Sleep to allow for data aquisition
-        time.sleep(1)
+        # Wait for the ADC sample to finish based on the sample rate plus a
+        # small offset to be sure (0.1 millisecond).
+        time.sleep(1.0/dataRate+0.0001)
         
         # Read two bytes from register 00, the ADC value
         value = self.bus.read_word_data(self.i2cAddress, 0x00) & 0xFFFF
--- aesrdevicelib/sensors/blue_esc.py
+++ aesrdevicelib/sensors/blue_esc.py
@@ -1,5 +1,4 @@
 from . import sensor
-import time
 
 
 class BlueESC(sensor.Sensor):
