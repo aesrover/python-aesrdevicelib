469697bf6d8a8b5d104bb7ea135f72340c1251c9
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-09-06 11:28:08 -0400

    Fix python styling of 'sensor' library

--- aesrdevicelib/sensors/sensor.py
+++ aesrdevicelib/sensors/sensor.py
@@ -1,10 +1,11 @@
 import smbus
 
+
 class Sensor(object):
-    def __init__(self, i2cAddress, bus= 1):
+    def __init__(self, i2cAddress, bus=1):
         self.bus = smbus.SMBus(bus)
         self.i2cAddress = i2cAddress
-        
+
         # Test if device is connected by transmitting just the slave address
         # and checking for an ACK from the device:
         self.bus.write_quick(i2cAddress)
