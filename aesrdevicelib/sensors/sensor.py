aa9079f291bc673683ad0b1e41b3ab1a19b2e316
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-09-06 11:30:00 -0400

    Add argument to make testing device optional in 'sensor'

--- aesrdevicelib/sensors/sensor.py
+++ aesrdevicelib/sensors/sensor.py
@@ -2,10 +2,11 @@ import smbus
 
 
 class Sensor(object):
-    def __init__(self, i2cAddress, bus=1):
+    def __init__(self, i2cAddress, bus=1, testDevice=True):
         self.bus = smbus.SMBus(bus)
         self.i2cAddress = i2cAddress
 
         # Test if device is connected by transmitting just the slave address
         # and checking for an ACK from the device:
-        self.bus.write_quick(i2cAddress)
+        if testDevice:
+            self.bus.write_quick(i2cAddress)
