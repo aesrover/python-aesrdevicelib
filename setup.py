a3bdcd61f9315390b865bde8e1afc42fcf90af7c
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-03-25 15:29:41 -0400

    Add python setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='python-aesrdevicelib',
+      version='1.0',
+      description='Hardware Device Library for AESR',
+      author='AESR',
+      url='http://http://www.aesrover.xyz/',
+      packages=find_packages()
+      )
