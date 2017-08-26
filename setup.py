da8e5c474bbac63c4d5448c66a8bccdfd5032dde
Lucien Gaitskell <lucieng@gaitskell.com>
2017-08-26 11:59:32 -0400

    Fix 'setup.py' formatting

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from setuptools import setup, find_packages
 
-setup(name='python-aesrdevicelib',
-      version='1.5',
-      description='Hardware Device Library for AESR',
-      author='AESR',
-      url='http://http://www.aesrover.xyz/',
-      packages=find_packages()
-      )
+setup(
+    name='python-aesrdevicelib',
+    version='1.5',
+    description='Hardware Device Library for AESR',
+    author='AESR',
+    url='http://http://www.aesrover.xyz/',
+    packages=find_packages()
+)
