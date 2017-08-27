5bc15ba7b125ba5b8437611edbcd61d12e1e3845
Lucien Gaitskell <lucieng@gaitskell.com>
2017-08-27 14:37:37 -0400

    Add generic voltage monitor class and implementation

--- aesrdevicelib/sensors/voltage_monitor.py
+++ aesrdevicelib/sensors/voltage_monitor.py
@@ -2,28 +2,43 @@ from .ads1x15 import ADS1115
 from ..base.transducer import Transducer
 
 
-class VoltageMonitorADS1115(ADS1115, Transducer):
-    def __init__(self, diff, *args, itype=None, other_data=None, resistor_p=None, resistor_n=None, adc_gain=1, adc_data_rate=None, **kwargs):
+class VoltageMonitor(Transducer):
+    def __init__(self, itype=None, gain: float=1., resistor_p=None, resistor_n=None, **other_data):
+        super().__init__("VOLT_MON", itype, **other_data)
+
+        self.gain = gain
+        if resistor_n is not None and resistor_p is not None:
+            self.res_gain = (resistor_p+resistor_n)/resistor_n
+        else:
+            self.res_gain = 1
+
+    def _convert(self, dir_volt):
+        return dir_volt * self.gain * self.res_gain
+
+    def _read_dir_voltage(self):
+        raise NotImplementedError
+
+    def read_voltage(self):
+        return self._convert(self._read_dir_voltage())
+
+    def read(self):
+        r = self._read_dir_voltage()
+        return {'dir_volt': r, 'volt': self._convert(r)}
+
+
+class VoltageMonitorADS1115(ADS1115, VoltageMonitor):
+    def __init__(self, diff, *args, itype=None, other_data=None, resistor_p=None, resistor_n=None, adc_gain=1,
+                 adc_data_rate=None, **kwargs):
         super().__init__(*args, **kwargs)
         if other_data is None:
             other_data = {}
-        Transducer.__init__(self, "PRES", itype, **other_data)
-
-        self.gain = (4.096/adc_gain)/(pow(2, 15)-1)
-        if resistor_n is not None and resistor_p is not None:
-            self.gain *= (resistor_p+resistor_n)/resistor_n
+        gain = (4.096 / adc_gain) / (pow(2, 15) - 1)
+        VoltageMonitor.__init__(self, itype, gain, resistor_p=resistor_p, resistor_n=resistor_n, **other_data)
 
         self.start_adc_difference(diff, adc_gain, adc_data_rate)  # Start continuous adc conversion
 
-    def _convert(self, dir_volt):
-        return dir_volt * self.gain
-
-    def read_voltage(self):
-        return self._convert(self.get_last_result())
-
-    def read(self):
-        r = self.get_last_result()
-        return {'dir_volt': r, 'volt': self._convert(r)}
+    def _read_dir_voltage(self):
+        return self.get_last_result()
 
     def close(self):
         self.stop_adc()
