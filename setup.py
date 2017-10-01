51fe73457b4c39b52caf7d89aed27d008e4ddc44
Lucien Gaitskell <lucieng@gaitskell.com>
2017-09-30 23:22:43 -0400

    Version bump to '3.0'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-aesrdevicelib',
-    version='2.0',
+    version='3.0',
     description='Hardware Device Library for AESR',
     author='AESR',
     url='http://http://www.aesrover.xyz/',
